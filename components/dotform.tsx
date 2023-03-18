ade1b08fb0408ac2f2738fc6f114693a841d51ce
Arnaldo Carrillo <arnaldocarrillo@Arnaldos-MacBook-Air.local>
2023-03-18 18:43:01 -0500

    adding truck image

--- components/dotform.tsx
+++ components/dotform.tsx
@@ -12,15 +12,23 @@ const DotForm: React.FC<DotFormProps> = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="dot-number">Enter DOT Number:</label>
-      <input
-        type="text"
-        id="dot-number"
-        value={dotNumber}
-        onChange={(e) => setDotNumber(e.target.value)}
-      />
-      <button type="submit">Submit</button>
+    <form onSubmit={handleSubmit} className="w-full max-w-sm">
+      <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
+        <input
+          type="text"
+          id="dot-number"
+          value={dotNumber}
+          onChange={(e) => setDotNumber(e.target.value)}
+          className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+          placeholder="Enter DOT Number"
+        />
+        <button
+          type="submit"
+          className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+        >
+          Submit
+        </button>
+      </div>
     </form>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,29 @@
 import Head from 'next/head';
-import DotForm from '../components/DotForm';
+import DotForm from '../components/dotform';
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
       <Head>
         <title>DOT Lookup</title>
         <meta name="description" content="A simple DOT lookup tool" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>DOT Lookup</h1>
-        <DotForm />
+      <main className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div className="md:flex">
+          <div className="md:flex-shrink-0">
+            <img
+              className="h-48 w-full object-cover md:h-full md:w-48"
+              src="/images/truck.jpg"
+              alt="Truck"
+            />
+          </div>
+          <div className="p-8">
+            <h1 className="block mt-1 text-lg leading-tight font-medium text-black">DOT Lookup</h1>
+            <DotForm />
+          </div>
+        </div>
       </main>
     </div>
   );
